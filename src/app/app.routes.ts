277189e3b027700956c19f66e6460c01d2f2a1cb
Dranhoc <christophe.h.leroy@gmail.com>
2026-03-24 15:31:19 +0100

    [ADD]guard notConnected

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-// import { Error404Component } from './features/error/error404/error404.component';
-// import { connectedGuard } from '@core/guards/connected.guard';
-// import { adminGuard } from '@core/guards/admin.guard';
+import { Error404Component } from './features/error/error404/error404.component';
+import { connectedGuard } from '@core/guards/connected.guard';
+import { adminGuard } from '@core/guards/admin.guard';
 import { HomePageComponent } from './features/home-page/home-page.component';
 
 export const routes: Routes = [
@@ -9,17 +9,17 @@ export const routes: Routes = [
     path: '',
     component: HomePageComponent,
   },
-  // {
-  //   path: 'auth',
-  //   loadChildren: () => import('./features/auth/auth.router').then((r) => r.routes),
-  // },
-  // {
-  //   path: 'user',
-  //   // canActivate: [connectedGuard],
-  //   loadChildren: () => import('./features/user/user.routes').then((r) => r.routes),
-  // },
-  // {
-  //   path: '**',
-  //   component: Error404Component,
-  // },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.router').then((r) => r.routes),
+  },
+  {
+    path: 'user',
+    canActivate: [connectedGuard],
+    loadChildren: () => import('./features/user/user.routes').then((r) => r.routes),
+  },
+  {
+    path: '**',
+    component: Error404Component,
+  },
 ];
