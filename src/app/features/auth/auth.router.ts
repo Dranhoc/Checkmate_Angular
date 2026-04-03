2c989c9241779888ba626a9f13aa6a739c365470
Dranhoc <christophe.h.leroy@gmail.com>
2026-04-03 14:06:18 +0200

    [ADD] guard not connected on auth/login

--- src/app/features/auth/auth.router.ts
+++ src/app/features/auth/auth.router.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AuthLoginPage } from './pages/auth-login-page/auth-login-page';
+import { notConnectedGuard } from '@core/guards/not-connected';
 
 export const routes: Routes = [
   {
     path: 'login',
+    canActivate: [notConnectedGuard],
     component: AuthLoginPage,
   },
   {
